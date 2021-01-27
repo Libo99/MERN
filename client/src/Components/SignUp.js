1550e77d325361c2d1620986bca2b1cd067c561d
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-27 21:54:03 +0100

    made a signup form

---
+++ client/src/Components/SignUp.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../SignUp.css'
+
+
+
+const SignUp = () => {
+    return(
+        <div className="signup-container">
+            <form className="signup">
+                <label htmlFor="name">Name:</label>
+                <input onChange={e => e.target.value} type="text" id="name" name="name" required="required"/>
+
+                <label htmlFor="username">Username:</label>
+                <input onChange={e => e.target.value} type="text" id="username" name="username" required="required"/>
+
+                <label htmlFor="mail">Mail:</label>
+                <input onChange={e => e.target.value} type="email" id="mail" name="mail" required="required"/>
+
+                <label htmlFor="password">Password:</label>
+                <input onChange={e => e.target.value} type="password" id="password" name="password" required="required"/>
+                <button type="submit">Sign up</button>
+            </form>
+        </div>
+    );
+};
+
+
+export default SignUp;
