c638fb4671ef5c6d8dec9a7a14dae12f4ea52117
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-29 21:25:04 +0100

    Added controller and updated routes

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,26 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../Models/User');
+const UserController = require('../controllers/UserController');
 
 
-router.get('/', (req, res) => {
-    User.find()
-    .then(data => res.json(data))
-    .catch(err => res.status(404).json({msg: 'no user found'}));
-});
+router.get('/', UserController.getUser);
 
-router.post('/', (req, res) => {
-    const user = new User({
-      name: req.body.name.toString().trim(),
-      username: req.body.username.toString().trim(),
-      mail: req.body.mail.toString().trim(),
-      password: req.body.password.toString().trim(),
-      created: Date.now()
-    });
-
-    user.save()
-    .then(data => res.json(data));
-});
+router.post('/', UserController.createUser);
 
 
 
---
+++ server/controllers/UserController.js
@@ -0,0 +1,19 @@
+const User = require("../Models/User");
+
+exports.getUser = (req, res) => {
+  User.find()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(404).json({ msg: "no user found" }));
+};
+
+exports.createUser = (req, res) => {
+  const user = new User({
+    name: req.body.name.toString().trim(),
+    username: req.body.username.toString().trim(),
+    mail: req.body.mail.toString().trim(),
+    password: req.body.password.toString().trim(),
+    created: Date.now(),
+  });
+
+  user.save().then((data) => res.json(data));
+};
