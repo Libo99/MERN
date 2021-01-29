a1fbebd21058abafa1ef13420916f72c1e205da3
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-29 23:06:47 +0100

    formatted adminpage

--- client/src/Components/Admin/AdminPage.js
+++ client/src/Components/Admin/AdminPage.js
@@ -1,36 +1,30 @@
-import React, { useEffect, useState } from 'react';
-
-
+import React, { useEffect, useState } from "react";
 
 const AdminPage = () => {
-const [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
 
-useEffect(()  => {
-const url = 'http://localhost:6592/user';
+  useEffect(() => {
+    const url = "http://localhost:6592/user";
 
-const getData = async () => {
-const res = await fetch(url);
-const data = await res.json();
-console.log(data)
-setUser(data);
-}
+    const getData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setUser(data);
+    };
 
-getData();
+    getData();
+  }, []);
 
-
-
-
- 
-}, [])
-
-    return (
-      <div>
-        <ul>
-          {user.map((user) => <li key={`${user._id}`}>{`${user.username}`}</li>)}
-        </ul>
-      </div>
-    );
-}
-
+  return (
+    <div>
+      <ul>
+        {user.map((user) => (
+          <li key={`${user._id}`}>{`${user.username}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default AdminPage;
