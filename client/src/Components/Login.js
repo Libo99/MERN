8c165c65aefe037e468e91f11cf7b20e80b87095
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-31 18:34:40 +0100

    added state in login page

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Login.css";
 
 const Login = () => {
+
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="Login-container">
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" name="username" id="username" onChange={(e) => setUserName(e.target.value)}/>
 
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" onChange={e => setPassword(e.target.value)}/>
 
         <button type="submit">Login</button>
       </form>
