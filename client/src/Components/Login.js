31e8774daf9d747c66c3694259f7dba821257af3
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-06 21:52:18 +0100

    Added some more css to the login form and added classnames for the h1 and the div tag containing everything

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -7,8 +7,8 @@ const Login = () => {
   const [password, setPassword] = useState('');
 
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="Login">
+      <h1 className="login-header">Login</h1>
       <div className="Login-container">
         <form>
           <label htmlFor="username">Username</label>
