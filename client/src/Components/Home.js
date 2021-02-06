0adf22a0da5fa0c11d27842e11512f3ff0b62047
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-06 22:30:34 +0100

    added a home css file and added some styling, aslo added a class to div

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import '../Home.css'
 
 
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="Home-container">
+      <h1>Home</h1>
     </div>
   );
 };
