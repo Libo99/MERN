4b155b29d90e912bb3a28364fa9f354e5cc08dad
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-09 15:37:10 +0100

    added some content on the homepage

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -6,7 +6,16 @@ import '../Home.css'
 const Home = () => {
   return (
     <div className="Home-container">
-      <h1>Home</h1>
+      <div className="hero-header">
+        <h3 className="hero-text">Welcome</h3>
+        <button className="btn">
+          <a href="SignUp">Hej</a>
+        </button>
+      </div>
+      <div className="About">
+        <h1 className="about-header">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, sit vel. At in atque tempora vel! Ipsa saepe quo repellat animi ab sunt nam quibusdam.</p>
+      </div>
     </div>
   );
 };
