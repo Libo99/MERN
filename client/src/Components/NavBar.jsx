54eec3437636d405002586baf9dd6187ed32abeb
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-09 17:38:23 +0100

    changed the positions of sign up and login in the navbar and made space between sign and up

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -9,11 +9,11 @@ const NavBar = () => {
         <Link to="/">
           <li>Home</li>
         </Link>
-        <Link to="/signup">
-          <li>Signup</li>
-        </Link>
         <Link to="login">
           <li>Login</li>
+        </Link>
+        <Link to="/signup">
+          <li>Sign up</li>
         </Link>
         <Link to="admin">
           <li>Admin</li>
