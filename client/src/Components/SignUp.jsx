79a850dbab707a1699e14bd69a6921724d4c8e29
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-12 22:09:42 +0100

    formatted code in signup component

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,53 +1,74 @@
 import React, { useState } from "react";
 import "./SignUp.css";
-import axios from 'axios';
+import axios from "axios";
 
 const SignUp = () => {
+  const [nameReq, setName] = useState("");
+  const [usernameReq, setUserName] = useState("");
+  const [mailReq, setMail] = useState("");
+  const [passwordReq, setPassword] = useState("");
 
-const [nameReq, setName] = useState('');
-const [usernameReq, setUserName] = useState('');
-const [mailReq, setMail] = useState('');
-const [passwordReq, setPassword] = useState('');
+  const user = {
+    name: nameReq,
+    username: usernameReq,
+    mail: mailReq,
+    password: passwordReq,
+  };
 
-const user = {
-  name: nameReq,
-  username: usernameReq,
-  mail: mailReq,
-  password: passwordReq,
-};
+  const url = "http://localhost:6592/user";
 
-
-
-const url = "http://localhost:6592/user";
-
-const register = (e) => {
+  const register = (e) => {
     e.preventDefault();
     axios.post(url, user).then((res) => console.log(res));
-}
+  };
 
   return (
     <div className="container">
-    <div className="signup">
-      <div className="header">
-        <h1>Sign up</h1>
+      <div className="signup">
+        <div className="header">
+          <h1>Sign up</h1>
+        </div>
+        <div className="form-container">
+          <form className="form">
+            <label htmlFor="name">Name:</label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              id="name"
+              name="name"
+              required="required"
+            />
+
+            <label htmlFor="username">Username:</label>
+            <input
+              onChange={(e) => setUserName(e.target.value)}
+              type="text"
+              id="username"
+              name="username"
+              required="required"
+            />
+
+            <label htmlFor="mail">Mail:</label>
+            <input
+              onChange={(e) => setMail(e.target.value)}
+              type="email"
+              id="mail"
+              name="mail"
+              required="required"
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              name="password"
+              required="required"
+            />
+            <button onClick={register}>Sign up</button>
+          </form>
+        </div>
       </div>
-    <div className="form-container">
-      <form className="form">
-        <label htmlFor="name">Name:</label>
-        <input onChange={(e) => setName(e.target.value)} type="text" id="name" name="name" required="required" />
-
-        <label htmlFor="username">Username:</label>
-        <input onChange={(e) => setUserName(e.target.value)} type="text" id="username" name="username" required="required"/>
-
-        <label htmlFor="mail">Mail:</label>
-        <input onChange={(e) => setMail(e.target.value)} type="email" id="mail" name="mail" required="required"/>
-
-        <label htmlFor="password">Password:</label>
-        <input onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" required="required" />
-        <button onClick={register}>Sign up</button>
-      </form>
-    </div>
-    </div>
     </div>
   );
 };
