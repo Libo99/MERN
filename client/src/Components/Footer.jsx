cfd3cc0a8a91d6e45e42856c752940044982fbfc
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-13 19:31:46 +0100

    removed the footer wrapper from the footer jsx and css and unnecassary css from the admin.css

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -4,12 +4,10 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-wrapper">
         <div className="footer-content">
           <p>Copyright © 2021, Rage Media - All Rights Reserved </p>
         </div>
       </div>
-    </div>
   );
 };
 
