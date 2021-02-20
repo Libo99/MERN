f4804c817b63f06ddf9a27a5160e9105586d8541
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-20 19:34:50 +0100

    added the route for the update function

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserController = require('../controllers/UserController');
+const UserController = require("../controllers/UserController");
 
+router.get("/", UserController.GetUser);
 
-router.get('/', UserController.GetUser);
+router.get("/:id", UserController.GetbyId);
 
-router.get('/:id', UserController.GetbyId);
+router.post("/", UserController.CreateUser);
+router.put("/:id", UserController.UpdateUser);
 
-router.post('/', UserController.CreateUser);
 router.delete("/:id", UserController.DeleteUser);
-
-
-
 
 module.exports = router;
