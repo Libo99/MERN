7365d8b0d77fc6514c621f09c5af180663982329
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-22 23:12:18 +0100

    did some clean up in the login component so that I can implement login functionality

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,36 +1,40 @@
 import React, { useState } from "react";
 import "./Login.css";
-import GoogleLogin from "react-google-login";
-import axios from "axios";
+// import GoogleLogin from "react-google-login";
+// import axios from "axios";
 
 const Login = () => {
-  const [username, setUserName] = useState("");
+  const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
-  const url = "http://localhost:6592/user";
+  const url = "http://localhost:6592/user/login";
 
-  const GoogleSuccess = (res) => {
-    console.log(res);
-    axios.post(url, {
-      data: {tokenId: res.tokenId}
-    }).then(res => console.log(res))
-    ;
-  };
-  const GoogleError = (res) => {
-    console.log("Sign in was unsuccessful");
-  };
+  // const GoogleSuccess = (res) => {
+  //   console.log(res);
+  //   axios.post(url, {
+  //     data: {tokenId: res.tokenId}
+  //   }).then(res => console.log(res))
+  //   ;
+  // };
+  // const GoogleError = (res) => {
+  //   console.log("Sign in was unsuccessful");
+  // };
+
+  const Handechange = (e) => {
+  e.preventDefault();
+  }
 
   return (
     <div id="container">
       <div className="Login">
         <h1 className="login-header">Login</h1>
         <div id="form-container">
-          <form>
-            <label htmlFor="username">Username:</label>
+          <form onSubmit={Handechange}>
+            <label htmlFor="mail">Mail:</label>
             <input
               type="text"
-              name="username"
-              id="username"
-              onChange={(e) => setUserName(e.target.value)}
+              name="mail"
+              id="mail"
+              onChange={(e) => setMail(e.target.value)}
             />
 
             <label htmlFor="password">Password:</label>
@@ -43,7 +47,7 @@ const Login = () => {
 
             <button type="submit">Login</button>
 
-            <GoogleLogin
+            {/* <GoogleLogin
               clientId="311552585779-lc65857lq7vdnkosuf1a2agsc6n40mab.apps.googleusercontent.com"
               render={(renderProps) => (
                 <button onClick={renderProps.onClick}>Google Login</button>
@@ -52,7 +56,7 @@ const Login = () => {
               onSuccess={GoogleSuccess}
               onFailure={GoogleError}
               cookiePolicy={"single_host_origin"}
-            />
+            /> */}
           </form>
         </div>
       </div>
