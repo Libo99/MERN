803ff98f3c46544f25bd21f6dd22434995e84b53
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-03-03 17:33:22 +0100

    added about again

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Home.css";
 import Header from "./Header";
-// import About from "./About";
+import About from "./About";
 
 const Home = () => {
   return (
     <div className="Home-container">
       <Header />
-      {/* <About /> */}
+      <About />
     </div>
   );
 };
