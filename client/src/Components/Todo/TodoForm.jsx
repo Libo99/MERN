edfff727dbd4a2b924f3bcdf09f1cf16b821e9a9
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-03-03 19:21:08 +0100

    made the form auto to it the todos wont overflow and did some changes for the todo form

--- client/src/Components/Todo/TodoForm.jsx
+++ client/src/Components/Todo/TodoForm.jsx
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
-import './TodoForm.css'
-import TodoList from './TodoList';
+import React, { useState } from "react";
+import "./TodoForm.css";
+import TodoList from "./TodoList";
 
 const TodoForm = () => {
-    const [todo, setTodo] = useState('');
-    const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([{
+    text: "Add a todo",
+    completed: false
+  }]);
 
-    const Addtodo = (e) => {
-        setTodos([...todos, {
-            id: todos.length,
-            text: todo,
-            completed: false
-        }])
-    }
-    const Handlesubmit = (e) => {
-        e.preventDefault();
-        if(!todo) return;
-        Addtodo();
-        setTodo('');
-    }
-    return (
-      <div className="todo">
-        <div className="todo-container">
-          <div className="todoform">
+  const Addtodo = (e) => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length,
+        text: todo,
+        completed: false,
+      },
+    ]);
+  };
+
+  const Handlesubmit = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    Addtodo();
+    setTodo("");
+  };
+  return (
+    <div className="todo">
+      <div className="todo-container">
+        <div className="todoform">
           <TodoList todos={todos} />
-            <form className="formtodo" onSubmit={Handlesubmit}>
-              <label className="todolabel" htmlFor="todo">
-                Add todo
-              </label>
-              <input
-                type="text"
-                id="todo"
-                onChange={(e) => setTodo(e.target.value)}
-                value={todo}
-              />
-              
-            </form>
-          </div>
+          <form className="formtodo" onSubmit={Handlesubmit}>
+            <label className="todolabel" htmlFor="todo">
+              Add todo
+            </label>
+            <input
+              type="text"
+              id="todo"
+              onChange={(e) => setTodo(e.target.value)}
+              value={todo}
+            />
+          </form>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default TodoForm
+export default TodoForm;
--- client/src/Components/Todo/TodoList.jsx
+++ client/src/Components/Todo/TodoList.jsx
@@ -5,14 +5,22 @@ import "./TodoList.css";
 function TodoList(props) {
     const {todos} = props;
     return (
-        <div className="list-container">
-            <ul className="todolist">
-                {todos.map((todo) => 
-                    <li className="todos" key={todo.id} style={{textDecoration: todo.completed ? "line-through" : ""}}>{todo.text}</li>
-                )}
-            </ul>
-        </div>
-    )
+      <div className="list-container">
+        <ul className="todolist">
+          {todos.map((todo) => (
+            <div className="todos">
+              <li 
+                key={todo.id}
+                style={{ textDecoration: todo.completed ? "line-through" : "", color: 'black', fontSize: '1.3rem' }}
+              >
+                {todo.text}
+              </li>
+              <button className="todobtn" onclick={todo.completed = !todo.completed}>completed</button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
 }
 
 export default TodoList
