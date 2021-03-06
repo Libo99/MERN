28fd71714a03369a961778244d13023cc568c0a4
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-03-06 21:55:02 +0100

    changed the classnames for the h3 element and p element

--- client/src/Components/Home/About.jsx
+++ client/src/Components/Home/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.aboutme}>
-          <h3 className={styles.h3}>About me</h3>
-          <p className={styles.p}>
+          <h3 className={styles.imgtitle}>About me</h3>
+          <p className={styles.imgtext}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, sit
             vel. At in atque tempora vel! Ipsa saepe quo repellat animi ab sunt
             nam quibusdam.
