7db5d92b0fcd653bc21bdab608b5d87e0267490c
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-03-06 21:42:21 +0100

    formatted the home and header components

--- client/src/Components/Home/Header.jsx
+++ client/src/Components/Home/Header.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./Home.module.css";
 
-
-
 const Header = () => {
   const { isAuthenticated, user } = useAuth0();
 
   return (
     <div className={styles.heroheader}>
       {isAuthenticated ? (
-        <h1 >Welcome {user.name}</h1>
+        <h1>Welcome {user.name}</h1>
       ) : (
         <h3 className={styles.herotext}>Welcome</h3>
       )}
